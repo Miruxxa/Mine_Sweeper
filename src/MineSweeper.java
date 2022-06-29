ec9a83e7f60b751ba5008f8b73f350ac9a25aad6
Myroslav Panchenko <panchenkomiroslav@gmail.com>
2022-06-29 12:47:38 +0300

    add mouse adapter + LeftButton press

--- src/MineSweeper.java
+++ src/MineSweeper.java
@@ -1,5 +1,8 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
 import sweeper.Box;
 import sweeper.Coord;
 import sweeper.Game;
@@ -46,6 +49,17 @@ public class MineSweeper extends JFrame {
 
             }
         };
+        panel.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                int x = e.getX() / IMAGE_SIZE;
+                int y = e.getY() / IMAGE_SIZE;
+                Coord coord = new Coord ( x , y );
+                if (e.getButton() == MouseEvent.BUTTON1)
+                    game.pressLeftButton (coord);
+                panel.repaint();
+            }
+        });
 
         panel.setPreferredSize(new Dimension(
                 Ranges.getSize().x * IMAGE_SIZE ,
